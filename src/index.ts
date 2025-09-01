77886646e9be7e5558dca793f33faefbb9118eaa
Mikaël Sévigny <sevignator@hotmail.com>
2025-09-01 07:24:11 -0400

    Add solution to 01-12

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", (req, res) => {
+  res.status(200);
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.send("OK");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
