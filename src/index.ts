fc70c6688f913315d575bf1c3c74d4475e5f9dd0
Mikaël Sévigny <sevignator@hotmail.com>
2025-09-01 08:07:55 -0400

    Add solution to 02-01

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 
 app.get("/healthz", (req, res) => {
   res.status(200);
@@ -15,3 +16,18 @@ app.get("/healthz", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+function middlewareLogResponses(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) {
+  res.on("finish", () => {
+    if (res.statusCode !== 200) {
+      console.log(
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`,
+      );
+    }
+  });
+  next();
+}
