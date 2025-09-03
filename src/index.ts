62e94f4aaf7eafb2c3abed6f899bd90dc310a96c
Mikaël Sévigny <sevignator@hotmail.com>
2025-09-02 20:27:21 -0400

    Add solution to 03-04

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,38 @@ import express, {
   type Request,
   type Response,
   type NextFunction,
-} from "express";
+} from 'express';
 
-import { config } from "./config.js";
+import { config } from './config.js';
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+app.use('/app', middlewareMetricsInc, express.static('./src/app'));
 app.use(middlewareLogResponses);
 
-app.get("/api/healthz", (req, res) => {
+app.get('/api/healthz', (req, res) => {
   res.status(200);
-  res.set("Content-Type", "text/plain; charset=utf-8");
-  res.send("OK");
+  res.set('Content-Type', 'text/plain; charset=utf-8');
+  res.send('OK');
 });
 
-app.get("/api/metrics", (req, res) => {
+app.get('/admin/metrics', (req, res) => {
   res.status(200);
-  res.set("Content-Type", "text/plain; charset=utf-8");
-  res.send(`Hits: ${config.fileserverHits}`);
+  res.set('Content-Type', 'text/html; charset=utf-8');
+  res.send(`<html>
+    <body>
+      <h1>Welcome, Chirpy Admin</h1>
+      <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+    </body>
+  </html>`);
 });
 
-app.get("/api/reset", (req, res) => {
+app.get('/admin/reset', (req, res) => {
   config.fileserverHits = 0;
 
-  res.redirect("/api/metrics");
+  res.redirect('/admin/metrics');
 });
 
 app.listen(PORT, () => {
@@ -38,12 +43,12 @@ app.listen(PORT, () => {
 function middlewareLogResponses(
   req: Request,
   res: Response,
-  next: NextFunction,
+  next: NextFunction
 ) {
-  res.on("finish", () => {
+  res.on('finish', () => {
     if (res.statusCode !== 200) {
       console.log(
-        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`,
+        `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`
       );
     }
   });
