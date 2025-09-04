4591aaea79426a21189a99d3206342f786bbbcab
Mikaël Sévigny <sevignator@hotmail.com>
2025-09-04 12:48:08 -0400

    Add solution to 04-02

--- src/index.ts
+++ src/index.ts
@@ -36,6 +36,41 @@ app.post('/admin/reset', (req, res) => {
   res.redirect('/admin/metrics');
 });
 
+app.post('/api/validate_chirp', (req, res) => {
+  const MAX_LENGTH = 140;
+  let body = '';
+
+  req.on('data', (chunk) => {
+    body += chunk;
+  });
+
+  req.on('end', () => {
+    try {
+      const parsedBody = JSON.parse(body);
+
+      if (!parsedBody.body) {
+        throw new Error('Something went wrong');
+      }
+
+      if (parsedBody.body.length > MAX_LENGTH) {
+        throw new Error('Chirp is too long');
+      }
+
+      res.status(200).send(
+        JSON.stringify({
+          valid: true,
+        })
+      );
+    } catch (error) {
+      res.status(400).send(
+        JSON.stringify({
+          error: error instanceof Error ? error.message : error,
+        })
+      );
+    }
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
