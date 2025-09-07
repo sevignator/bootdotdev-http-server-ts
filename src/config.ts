0574174a626f6d2be262fcd0231ba1cd869a9ad9
Mikaël Sévigny <sevignator@hotmail.com>
2025-09-06 21:07:46 -0400

    Add solution to 06-03

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,15 @@
+process.loadEnvFile();
+
+if (!process.env.DB_URL) {
+  throw new Error('Please add a `DB_URL` value to your .env file.');
+}
+
 type APIConfig = {
+  dbURL: string;
   fileserverHits: number;
 };
 
 export const config: APIConfig = {
+  dbURL: process.env.DB_URL,
   fileserverHits: 0,
 };
---
+++ src/db/queries/users.ts
@@ -0,0 +1,12 @@
+import { db } from '../index.js';
+import { type NewUser, users } from '../schema.js';
+
+export async function createUser(user: NewUser) {
+  const [result] = await db
+    .insert(users)
+    .values(user)
+    .onConflictDoNothing()
+    .returning();
+
+  return result;
+}
